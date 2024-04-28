e5bf3fa0576acb732ad47c12349bce619ddb0908
BGurunathGoud <93756528+BGurunathGoud@users.noreply.github.com>
2024-04-28 14:33:24 +0530

    Add files via upload

---
+++ src/components/Button.js
@@ -0,0 +1,116 @@
+import { useContext } from "react";
+import { CalcContext } from '../context/CalcContext'
+
+//Sign operators
+const getStyleName = btn => {
+  const className = {
+    '=': 'equals',
+    'x': 'opt',
+    '-': 'opt',
+    '+': 'opt',
+    '/': 'opt',
+  }
+  return className[btn]
+}
+
+const Button = ({ value }) => {
+  const { calc, setCalc } = useContext(CalcContext);
+
+  // When User click's on dot button
+  const commaClick = () => {
+    setCalc({
+      ...calc,
+      num: !calc.num.toString().includes('.') ? calc.num + value : calc.num
+    });
+  }
+  //When User click's C it need to clear and set output to 0
+  const resetClick = () => {
+    setCalc({ sign: '', num: 0, res: 0 })
+  }
+  // Here User can click on numbers
+  const handleClickButton = () => {
+    const numberString = value.toString()
+
+    let numberValue;
+    if(numberString === '0' && calc.num === 0) {
+      numberValue = "0"
+    } else {
+      numberValue = Number(calc.num + numberString)
+    }
+
+    setCalc({
+      ...calc,
+      num: numberValue
+    })
+  }
+  // Here operation are operated when user click's
+  const signClick = () => {
+    setCalc({
+      sign: value,
+      res: !calc.res && calc.num ? calc.num : calc.res,
+      num: 0
+    })
+  }
+  // Below shows how the equal button works after User click's equals button
+  const equalsClick = () => {
+    if(calc.res && calc.num) {
+      const math = (a, b, sign) => {
+        const result = {
+          '+': (a, b) => a + b,
+          '-': (a, b) => a - b,
+          'x': (a, b) => a * b,
+          '/': (a, b) => a / b,
+        }
+        return result[sign](a, b);
+      }
+      setCalc({
+        res: math(calc.res, calc.num, calc.sign),
+        sign: '',
+        num: 0
+      })
+    }
+  }
+  // Below is for Percentage for given numbers
+  const persenClick = () => {
+    setCalc({
+      num: (calc.num / 100),
+      res: (calc.res / 100),
+      sign: ''
+    })
+  }
+  // Here when User click's on "+-"(inverted) button
+  const invertClick = () => {
+    setCalc({
+      num: calc.num ? calc.num * -1 : 0,
+      res: calc.res ? calc.res * -1 : 0,
+      sign: ''
+    })
+  }
+  
+
+  const handleBtnClick = () => {
+    
+    const results = {
+      '.': commaClick,
+      'C': resetClick,
+      '/': signClick,
+      'x': signClick,
+      '-': signClick,
+      '+': signClick,
+      '=': equalsClick,
+      '%': persenClick,
+      '+-': invertClick
+    }
+    if(results[value]) {
+      return results[value]()
+    } else {
+      return handleClickButton()
+    }
+  }
+
+  return (
+    <button onClick={handleBtnClick} className={`${getStyleName(value)} button`}>{value}</button>
+  )
+}
+
+export default Button
